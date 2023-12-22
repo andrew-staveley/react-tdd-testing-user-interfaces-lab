f04a9b380f291e438f3e52b11d0670867017400f
Andrew <andrewstaveley14@gmail.com>
2023-12-21 19:19:15 -0500

    Completed ASsignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Andrew</h1>
+      <img src="https://media.newyorker.com/photos/59095bb86552fa0be682d9d0/master/w_1920,c_limit/Monkey-Selfie.jpg" alt="Profile picture" />
+    </div>
+  )
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,4 +3,21 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+test("Displays a top level heading with the text `Hi, I'm _____`", () => {
+    render(<App />)
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    })
+    expect(topLevelHeading).toBeInTheDocument();
+})
+
+test("Displays a top level image with a picture with an alt of `Profile Picture`.", () => {
+    render(<App />)
+    const profilePhoto = screen.getByRole("image", {
+        name: /profile picture/i,
+        exact: false,
+    })
+    expect(profilePhoto).toBeInTheDocument();
+})
